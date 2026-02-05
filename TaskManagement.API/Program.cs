3382a62b4eb04c734c86ebe69711fecbe75d154a
tgcoder-17 <tushar1710vg@gmail.com>
2026-02-05 11:28:41 +0530

    Removed WeatherForcast Dummy api. Included swagger in production environment.

--- TaskManagement.API/Program.cs
+++ TaskManagement.API/Program.cs
@@ -26,11 +26,11 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.  
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 app.UseSerilogRequestLogging(options =>
 {
